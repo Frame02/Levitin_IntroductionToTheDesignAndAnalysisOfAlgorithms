c31beda88feaeaa4bc3407792df78807a70ad5c2
Frame02 <Frame02@users.noreply.github.com>
2015-08-05 10:43:43 +0530

    Compute Fibonacci Numbers using Bottomup Dynamic Programming

--- Ch08DynamicProgramming/DynamicProgramming/src/main/java/com/srikar/algorithms/levitin/FibonacciDynamic.java
+++ Ch08DynamicProgramming/DynamicProgramming/src/main/java/com/srikar/algorithms/levitin/FibonacciDynamic.java
@@ -1,13 +1,29 @@
 package com.srikar.algorithms.levitin;
 
-/**
- * Hello world!
- *
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+
+ f(n) = f(n-1) + f(n-2) if n > 1
+ f(0) = 0 and f(1) = 1
+
  */
-public class FibonacciDynamic 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class FibonacciDynamic {
+
+    public static long fibonacci(int n) {
+        List<Integer> fibonacciList = new ArrayList<>(n + 1);
+        fibonacciList.add(0);
+        fibonacciList.add(1);
+        for (int i = 2; i <= n; ++i) {
+            fibonacciList.add(fibonacciList.get(i - 1) + fibonacciList.get(i - 2));
+        }
+        return fibonacciList.get(n);
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 15; ++i) {
+            System.out.println("Fibonacci(" + i +"): " + fibonacci(i));
+        }
     }
 }
